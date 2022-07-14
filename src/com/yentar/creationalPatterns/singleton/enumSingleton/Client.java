7e87333345274975d0afd518979e002acf266b0d
Mehmet Yentar <Mehmet.Yentar@gmail.com>
2022-07-14 13:40:03 +0200

    multi-thread client created for enumSingleton

--- src/com/yentar/creationalPatterns/singleton/enumSingleton/Client.java
+++ src/com/yentar/creationalPatterns/singleton/enumSingleton/Client.java
@@ -1,8 +1,14 @@
 package com.yentar.creationalPatterns.singleton.enumSingleton;
 
-public class Client{
-	
-	public static void main (String args []) {
-		EnumSingleton.INSTANCE.inkrementValue();
-	}
+public class Client extends Thread {
+    public static void main(String[] args) {
+        new Client().start();
+        new Client().start();
+    }
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            EnumSingleton.INSTANCE.inkrementValue();
+        }
+    }
 }
